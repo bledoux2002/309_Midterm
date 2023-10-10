eb41d392bef2fcd87229798aa0d005bf21b13057
Benjamin Ledoux <bledoux2002@gmail.com>
2023-10-10 17:17:40 -0400

    added countdown
    redirected start test to countdown timer, which shows timer on screen then begins test

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+namespace TMPro.Fitts
+{
+    public class Countdown : MonoBehaviour
+    {
+        public float _startTime;
+        public UnityEvent function;
+        public TextMeshProUGUI _countdownUI;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            _startTime = 3f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            _startTime = _startTime - Time.deltaTime;
+
+            _countdownUI.text = _startTime.ToString();
+
+            if (_startTime <= 0f)
+            {
+                function.Invoke();
+                gameObject.SetActive(false);
+                return;
+            }
+        }
+
+        public void CountdownStart()
+        {
+            gameObject.SetActive(true);
+            _startTime = 3f;
+        }
+    }
+}
